c5d5108120454ac511f4fe89689f7549e19bd588
Jil Patel <jilpatel0310@gmail.com>
2025-01-20 19:00:39 +0530

    Add files via upload

---
+++ jil3python.py
@@ -0,0 +1,26 @@
+# 21. Calculate the net sales
+grosssales=500
+discount=(10*grosssales)/100
+netsales=grosssales-discount
+print(netsales)
+# 22. Calculate the average of three numbers
+a=int(input("enter the first marks"))
+b=int(input("enter the second marks"))
+c=int(input("enter the third marks"))
+avg=(a+b+c)/3
+print(avg)
+# 23. Swap the two numbers
+a=int(input("enter first no."))
+b=int(input("enter the second no."))
+c=a
+a=b
+b=c
+print(a,b)
+# 24. Addition , Subtration , Multiplication , Divition
+a=2
+b=3
+c=a+b
+d=a-b
+e=a*b 
+f=b/a 
+print(c,d,e,f)
